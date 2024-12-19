9c92ba15a24fa923233fea77bf727b7a7f057fbc
RodriCamio <rodricamio94@gmail.com>
2024-12-19 16:58:05 -0300

    Variables en JS

---
+++ 04JavaScript/js/02.js
@@ -0,0 +1,26 @@
+// Variables con Let 
+// no hay diferencia entre var y let
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignar valor.
+
+let disponible; // Iniciamos la variable pero sin valor.
+
+//no hay que declarar el tipo de dato, y no es importante cerrar con punto y coma.
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true;
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // asignar multiples valores
+
+// var 1disponibles;   no se puede comenzar una variable con un numero
+// var +disponibles;   no se puede comenzar una variable con un simbolo tampoco
+
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD'; // underscore
+let nombreProducto = 'Monitor HD'; // Camel Case
+let NombreProducto = 'Monitor HD'; // Pascal Case
+let nombreproducto = 'Monitor HD';
---
+++ 04JavaScript/js/03.js
@@ -0,0 +1,25 @@
+// Variables con 
+
+const producto = 'Audifonos Gamer'; // Iniciar variable y asignar valor.
+
+let disponible; // no puedes iniciarlizarlo sin valor
+
+//no hay que declarar el tipo de dato, y no es importante cerrar con punto y coma.
+
+producto = true; // No se puede reasignar
+
+disponible = true;
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // asignar multiples valores
+
+// var 1disponibles;   no se puede comenzar una variable con un numero
+// var +disponibles;   no se puede comenzar una variable con un simbolo tampoco
+
+
+// Estilos para las variables
+const nombre_producto = 'Monitor HD'; // underscore
+const nombreProducto = 'Monitor HD'; // Camel Case
+const NombreProducto = 'Monitor HD'; // Pascal Case
+const nombreproducto = 'Monitor HD';
