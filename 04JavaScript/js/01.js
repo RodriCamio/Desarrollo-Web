ed920012e81b1ab577766be1706d0680b7582887
RodriCamio <rodricamio94@gmail.com>
2024-12-19 15:39:31 -0300

    Comenzando con JavaScript

---
+++ 04JavaScript/js/01.js
@@ -0,0 +1,28 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignar valor.
+
+var disponible; // Iniciamos la variable pero sin valor.
+
+//no hay que declarar el tipo de dato, y no es importante cerrar con punto y coma.
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // asignar multiples valores
+
+// var 1disponibles;   no se puede comenzar una variable con un numero
+// var +disponibles;   no se puede comenzar una variable con un simbolo tampoco
+
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // Camel Case
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD';
+
+
+console.log(producto); // es sencible a las mayusculas, por lo que hay que prestar atencion a la hora de definir variables 
