718f0dbd73522f3249dfc167259550aef721fad4
RodriCamio <rodricamio94@gmail.com>
2025-01-02 16:14:20 -0300

    Arreglos o Arrays

---
+++ 04JavaScript/js/14.js
@@ -0,0 +1,14 @@
+// Arreglos o Arrays
+const numeros = [10, 20, 30, 40, 50]; // array mas usado
+console.table(numeros);
+
+const meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'); // constructor
+console.log(meses);
+
+// Acceder a los valores de un arreglo
+console.log(numeros[4]); // debemos especifica el indice que queremos llamar. 
+console.log(numeros[2]);
+
+// Conocer la extension de un arreglo
+console.log(meses.length); // cantidad de elementos en el arreglo.
+
