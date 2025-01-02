74e30a1f6685d24ea3195cdcbb5eb960012035c3
RodriCamio <rodricamio94@gmail.com>
2025-01-02 14:22:44 -0300

    Modificar Objetos

--- 04JavaScript/js/10.js
+++ 04JavaScript/js/10.js
@@ -10,6 +10,13 @@ const producto = {
     disponible: true
 }
 
-console.log(producto);
+
 console.log(producto.precio);
 
+// Agregar nuevas propiedades
+producto.imagen = 'imagen.jpg';
+
+// Eliminar propiedades
+delete producto.disponible;
+
+console.log(producto);
