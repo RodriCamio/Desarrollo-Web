6573017971145efea33420c2c99ece5075cfa398
RodriCamio <rodricamio94@gmail.com>
2025-01-03 17:21:49 -0300

    Diferencias entre Metodos y Funciones

---
+++ 04JavaScript/js/17.js
@@ -0,0 +1,7 @@
+// Diferencias entre Metodos y Funciones
+
+const numero1 = 20;
+const numero2 = '20';
+
+console.log( parseInt(numero2) ); // parseInt() es una funcino porque tiene el nombre seguido de unos parentecis y tiene un valor.
+console.log( numero1.toString() ); // .toString() es un metodo porque empieza con un punto seguido del nombre de la funcion.
